86c4b1c704fff959d7f3b20302fdc66fa5033c49
nicolefrances <meneses.nicolefrances@gmail.com>
2024-06-20 14:39:40 +0800

    modify the database query and the connection to mongodb

--- backend/routes.js
+++ backend/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { getConnectedClient } = require("./database");
+const { ObjectId } = require("mongodb");
 
 const getCollection = () => {
     const client = getConnectedClient();
@@ -13,7 +14,7 @@ router.get("/todos", async (req, res) => {
     const collection = getCollection();
     const todos = await collection.find({}).toArray();
 
-    res.status(200).json({ mssg: "GET REQUEST TO /api/todos" });
+    res.status(200).json(todos);
 });
 
 //POST - create request 
@@ -21,19 +22,41 @@ router.post("/todos", async (req, res) => {
     const collection = getCollection();
     const { todo } = req.body;
 
+    if (!todo){
+        return res.status(400).json({ mssg: "error no to do found"});
+    }
+
+    todo = JSON.stringify(todo);
+
     const newTodo = await collection.insertOne({ todo, status: false });
 
     res.status(201).json({ todo, status: false, _id: newTodo.insertedId });
 });
 
 //DELETE - delete the info
-router.delete("/todos/:id", (req, res) => {
-    res.status(200).json({ mssg: "DELETE REQUEST TO /api/todos/:id" });
+router.delete("/todos/:id", async (req, res) => {
+    const collection = getCollection();
+    const _id = new ObjectId(req.params.id);
+
+    const deleteTodo = await collection.deleteOne({ _id });
+
+    res.status(200).json(deleteTodo);
 });
 
 //PUT - update the info 
-router.put("/todos/:id", (req, res) => {
-    res.status(200).json({ mssg: "PUT REQUEST TO /api/todos/:id" });
+router.put("/todos/:id", async (req, res) => {
+    const collection = getCollection();
+    const _id = new ObjectId(req.params.id);
+    const { status } = req.body;
+
+    if (typeof status !== "boolean"){
+        return res.status(400).json({ mssg: "invalid status"});
+    }
+
+    const updateTodo = await collection.updateOne({ _id }, { $set: { status: !status }}); 
+
+
+    res.status(200).json(updateTodo);
 });
 
 module.exports = router;
